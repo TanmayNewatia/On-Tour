7512a0f47ede8e34e8fd6422e7314a611a94c95a
TanmayNewatia <tr8497@srmist.edu.in>
2022-12-29 13:40:21 +0530

    Sign in Section made Responsive

--- script.js
+++ script.js
@@ -182,9 +182,15 @@ function openNav() {
   document.getElementById("mySidenav").style.width = "30%";
 }
 
-function changeIcon(anchor) {
+function changeIcon1(anchor) {
   var icon = anchor.querySelector("i");
   icon.classList.toggle('fa-bars');
   icon.classList.toggle('fa-xmark');
   anchor.closest('.side-nav-wrapper').classList.toggle('active');
+}
+
+function changeIcon2(anchor) {
+  var icon = anchor.querySelector("i");
+  icon.classList.toggle('fa-sign-in-alt');
+  icon.classList.toggle('fa-sign-out-alt');
 }

0efefae3454995accb494ebffc962754c87c8083
TanmayNewatia <tr8497@srmist.edu.in>
2023-01-06 19:47:00 +0530

    Sign In Sign Up Box Updated

--- script.js
+++ script.js
@@ -204,3 +204,30 @@ function changeIcon3(anchor) {
     element[i].classList.toggle("dark-mode");
   }
 }
+
+function su() {
+  var signin = document.getElementById("signincard");
+  var signup = document.getElementById("signupcard");
+  var sut = document.getElementById("sut");
+  var sit = document.getElementById("sit");
+  var i=sit.querySelector("span");
+  var u=sut.querySelector("span");
+  signin.classList.remove("show");
+  i.classList.remove("act");
+  signin.classList.add("collapse");
+  signup.classList.add("show");
+  u.classList.add("act");
+}
+
+function si() {
+  var signin = document.getElementById("signincard");
+  var signup = document.getElementById("signupcard");
+  var sut = document.getElementById("sut");
+  var sit = document.getElementById("sit");
+  var i=sit.querySelector("span");
+  var u=sut.querySelector("span")
+  signup.classList.remove("show");
+  u.classList.remove("act");
+  signin.classList.add("show");
+  i.classList.add("act");
+}
